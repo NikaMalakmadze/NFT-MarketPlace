b96a0b0ba4240b4c571fe8e3c8d0ceee21fddfdc
NikaMalakmadze <malakmadzenika0909@gmail.com>
2025-07-19 17:45:19 +0400

    updated db

--- app/extensions.py
+++ app/extensions.py
@@ -1,17 +1,9 @@
 
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.engine import Engine
 from flask_login import LoginManager
 from flask_limiter import Limiter
-from sqlalchemy import event
 
 db = SQLAlchemy()
 limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
 login_manager = LoginManager()
-
-@event.listens_for(Engine, "connect")
-def enforce_foreign_keys(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
